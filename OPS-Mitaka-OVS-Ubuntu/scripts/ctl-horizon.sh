0b27ad04426bbfb852001b502a57dc697b93f814
Tô Thành Công <tcvn1985@gmail.com>
2016-04-19 14:26:17 +0700

    script horizon
    script horizon

--- OPS-Mitaka-OVS-Ubuntu/scripts/ctl-horizon.sh
+++ OPS-Mitaka-OVS-Ubuntu/scripts/ctl-horizon.sh
@@ -38,7 +38,22 @@ sed -i "s/'can_set_password': False/'can_set_password': True/g" \
 /etc/openstack-dashboard/local_settings.py
 
 sed -i "s/_member_/user/g" /etc/openstack-dashboard/local_settings.py 
+sed -i "s/127.0.0.1/$CTL_MGNT_IP/g" /etc/openstack-dashboard/local_settings.py
+sed -i "s/http:\/\/\%s:5000\/v2.0/http:\/\/\%s:5000\/v3/g" \
+/etc/openstack-dashboard/local_settings.py
 
+cat << EOF >> /etc/openstack-dashboard/local_settings.py
+OPENSTACK_API_VERSIONS = {
+#    "data-processing": 1.1,
+    "identity": 3,
+    "volume": 2,
+    "compute": 2,
+}
+EOF
+
+sed -i "s/#OPENSTACK_KEYSTONE_DEFAULT_DOMAIN = 'default'/ \
+OPENSTACK_KEYSTONE_DEFAULT_DOMAIN = 'default'/g" \
+/etc/openstack-dashboard/local_settings.py 
 
 ## /* Restarting apache2 and memcached
 service apache2 restart
