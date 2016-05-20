4e13867192b1bf6d0f0a0f5f9023861bea113808
Tô Thành Công <tcvn1985@gmail.com>
2016-05-20 22:35:08 +0700

    fix create initial network for project admin
    fix create initial network for project admin

--- OPS-Mitaka-OVS-Ubuntu/scripts/ctl-create-network.sh
+++ OPS-Mitaka-OVS-Ubuntu/scripts/ctl-create-network.sh
@@ -15,27 +15,27 @@ neutron subnet-create ext-net --name ext-subnet --allocation-pool \
     start=172.16.69.30,end=172.16.69.39 --disable-dhcp \
     --gateway 172.16.69.1 172.16.69.0/24
     
-tenant_id=`openstack project show demo | egrep -w id | awk '{print $4}'`
+tenant_id=`openstack project show admin | egrep -w id | awk '{print $4}'`
 
 echocolor "Create the project network"
 sleep 3
-neutron net-create demo-net --tenant-id $tenant_id \
+neutron net-create admin-net --tenant-id $tenant_id \
     --provider:network_type gre
     
 
 echocolor "Create a subnet on the project network"
 sleep 3
-neutron subnet-create demo-net --name demo-subnet --gateway 192.168.10.1 \
+neutron subnet-create admin-net --name admin-subnet --gateway 192.168.10.1 \
     192.168.10.0/24
     
 echocolor "Create a project router"
 sleep 3
-neutron router-create demo-router
+neutron router-create admin-router
 
 echocolor "Add the project subnet as an interface on the router"
 sleep 3
-neutron router-interface-add demo-router demo-subnet
+neutron router-interface-add admin-router admin-subnet
 
 echocolor "Add a gateway to the external network on the router"
 sleep 3
-neutron router-gateway-set demo-router ext-net
+neutron router-gateway-set admin-router ext-net
