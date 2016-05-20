ef68ef9f41cade2d9b1a7cccdcf9c01cdebff18d
Tô Thành Công <tcvn1985@gmail.com>
2016-05-20 23:07:31 +0700

    fix dns in create initial network
    fix dns in create initial network

--- OPS-Mitaka-OVS-Ubuntu/scripts/ctl-create-network.sh
+++ OPS-Mitaka-OVS-Ubuntu/scripts/ctl-create-network.sh
@@ -13,20 +13,20 @@ echocolor "Create a subnet on the external network:"
 sleep 3
 neutron subnet-create ext-net --name ext-subnet --allocation-pool \
     start=172.16.69.30,end=172.16.69.39 --disable-dhcp \
-    --gateway 172.16.69.1 172.16.69.0/24
+    --dns-nameserver 8.8.4.4 --gateway 172.16.69.1 172.16.69.0/24
     
 tenant_id=`openstack project show admin | egrep -w id | awk '{print $4}'`
 
 echocolor "Create the project network"
 sleep 3
-neutron net-create admin-net --tenant-id $tenant_id \
+neutron net-create private-net --tenant-id $tenant_id \
     --provider:network_type gre
     
 
 echocolor "Create a subnet on the project network"
 sleep 3
-neutron subnet-create admin-net --name admin-subnet --gateway 192.168.10.1 \
-    192.168.10.0/24
+neutron subnet-create private-net --name private-subnet \
+    --dns-nameserver 8.8.8.8 --gateway 192.168.10.1 192.168.10.0/24
     
 echocolor "Create a project router"
 sleep 3
@@ -34,8 +34,15 @@ neutron router-create admin-router
 
 echocolor "Add the project subnet as an interface on the router"
 sleep 3
-neutron router-interface-add admin-router admin-subnet
+neutron router-interface-add admin-router private-subnet
 
 echocolor "Add a gateway to the external network on the router"
 sleep 3
 neutron router-gateway-set admin-router ext-net
+
+echocolor "Allow SSH, ICMP protocol"
+openstack security group rule create default --proto icmp
+openstack security group rule create default --proto tcp --dst-port 22
+
+
+
