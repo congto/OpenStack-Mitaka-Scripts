a7aa9cb97c941fa4c1411bcfd16b28189a8da897
Tô Thành Công <tcvn1985@gmail.com>
2016-12-08 18:45:39 +0700

    Cập nhật script vlan

--- OPS-Liberty-CentOS7/scripts/creat-VM.sh
+++
@@ -1,5 +0,0 @@
-selfservice=`openstack network list | awk '/selfservice/ {print $2}'`
-
-openstack server create --flavor m1.tiny --image cirros \
-    --nic net-id=$selfservice --security-group default vm6969
-    
