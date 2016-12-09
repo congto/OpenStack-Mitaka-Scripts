043c13a5faffb8f0d0252a96271e7b9b83e301f8
Tô Thành Công <tcvn1985@gmail.com>
2016-12-09 15:04:18 +0700

    cập nhật script tạo network, vm

--- Ubuntu-OVS-VLAN/scripts/ctl-create-network.sh
+++ Ubuntu-OVS-VLAN/scripts/ctl-create-network.sh
@@ -13,8 +13,9 @@ echocolor "Tạo provider network theo kiểu VLAN"
 sleep 3
 
 neutron net-create provider_VLAN --shared \
-  --provider:physical_network provider --provider:network_type vlan \
-  --provider:segmentation_id 101
+	--router:external True \
+  	--provider:physical_network provider --provider:network_type vlan \
+  	--provider:segmentation_id 101
 
  neutron subnet-create provider_VLAN 192.168.101.0/24 \
   	--name provider_subnet_VLAN101 \
@@ -22,5 +23,10 @@ neutron net-create provider_VLAN --shared \
   	--dns-nameserver 8.8.8.8 \
   	--allocation-pool start=192.168.101.20,end=192.168.101.60
 
-echocolor "Create VM"
+echocolor "Tao VM"
 nova boot --flavor m1.tiny --image cirros VM1
+
+echocolor "Tao private network su dung VXLAN"
+sleep 3
+neutron net-create private-net  --provider:network_type vxlan
+neutron subnet-create private-net --name private-net-subnet --gateway 172.16.10.1 172.16.10.0/24
